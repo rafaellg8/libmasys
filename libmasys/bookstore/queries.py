64d020ad94eecb66810139f7b0235ca69d1e8bf5
rafaellg8 <rafaellg8@correo.ugr.es>
2016-08-07 20:28:05 +0200

    queries

--- libmasys/bookstore/queries.py
+++ libmasys/bookstore/queries.py
@@ -1,7 +1,9 @@
 from bookstore.models import *
 
 def search(string):
-    querie = Libro.objects.filter(titulo__contains=string) or Libro.objects.filter(autor__contains=string) or Libro.objects.filter( genero__nombre__contains=string)
+    stringparts = string.split()
+    for value in stringparts or querie.count()>0:
+        querie = Libro.objects.filter(titulo__contains=value) or Libro.objects.filter(autor__contains=value) or Libro.objects.filter( genero__nombre__contains=value)
     return querie
 
 def searchTitle(string):
