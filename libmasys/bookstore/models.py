49ab26a5c73212608adea14f41a3c7db0631a11b
rafaellg8 <rafaellg8@correo.ugr.es>
2016-08-08 18:22:48 +0200

    cambio modelos

--- libmasys/bookstore/models.py
+++ libmasys/bookstore/models.py
@@ -35,7 +35,10 @@ class Recurso(models.Model):
     estanteria = models.CharField(blank=True, max_length=100)
     genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
     edicion = models.IntegerField(blank=True, null=True)
+    editorial = models.CharField(blank=True, max_length=250)
     dvd = models.BooleanField(default=False)
+    fecha = models.DateField(blank=True) #por defecto 7 dias despues de la creacion
+
     def __unicode__(self):
             return self.titulo
 
