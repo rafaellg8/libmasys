6e3db637f23f43eb76eba4549558bf3ca9f8551d
rafaellg8 <rafaellg8@correo.ugr.es>
2016-09-27 16:53:42 +0200

    heroku despliegue remoto

--- libmasys/tests.py
+++ libmasys/tests.py
@@ -70,63 +70,45 @@ Recursos
 # recurso = Recurso(titulo="Doraemon", autor="Disney",descripcion="",genero=genero,dvd=True)
 # recurso.save()
 
-
-"""
-FILOSOFIA
-"""
-genero = Genero(nombre="Filosofía y pensamiento")
-genero.save()
-
-wb2 = load_workbook('filo.xlsx')
-ws = wb2.active
-
-
-for row in ws['A2':'H125']:
-        recurso = Recurso(titulo=row[0].value,autor=row[1].value,anio=str(int(row[3].value)),editorial=row[4].value,genero=genero,codigo=row[7].value)
-        recurso.save()
-
-for row in ws['A138':'H155']:
-        recurso = Recurso(titulo=row[0].value,autor=row[1].value,anio=str(int(row[3].value)),editorial=row[4].value,genero=genero,codigo=row[7].value)
-        recurso.save()
-"""
-
-class Genero(models.Model):
-    nombre = models.CharField(blank=False, max_length=10)
-
-    def get(self):
-        return nombre
-
-    def __unicode__(self):
-            return self.nombre
-
-class Usuario(models.Model):
-    nombre = models.CharField(blank=False, max_length=15)
-    apellidos = models.CharField(blank=False, max_length=30)
-    phone_regex = RegexValidator(regex=r'^\d{9,15}$', message="Telefono debe ser formato: 99911122.")
-    telefono = models.CharField(validators=[phone_regex], blank=True,max_length=15) # validators should be a list
-    direccion = models.CharField(blank=True,max_length=100)
-
-    def __unicode__(self):
-            return self.nombre+" "+self.apellidos
-
-class Recurso(models.Model):
-    titulo = models.CharField(blank=False, max_length=100)
-    autor = models.CharField(blank=True, max_length=15)
-    descripcion = models.CharField(blank=True, max_length=100)
-    orden = models.CharField(blank=True, max_length=10)
-    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
-    edicion = models.IntegerField(blank=True, null=True)
-    dvd = models.BooleanField(default=False)
-    def __unicode__(self):
-            return self.titulo
-
-class Prestamo(models.Model):
-
-    fechaInicio = models.DateField(default=datetime.datetime.today)
-    fechaFin = models.DateField(default=datetime.datetime.today()+datetime.timedelta(days=7)) #por defecto 7 dias despues de la creacion
-    Recurso = models.OneToOneField(Recurso,unique=True)
-    usuarioPrestamo = models.OneToOneField(Usuario,unique=True)
-
-    def __unicode__(self):
-        return self.fechaFin.strftime("%Y-%m-%d")+" "+self.Recurso.__unicode__()+" "+self.usuarioPrestamo.__unicode__()
-"""
+#
+#
+# class Genero(models.Model):
+#     nombre = models.CharField(blank=False, max_length=10)
+#
+#     def get(self):
+#         return nombre
+#
+#     def __unicode__(self):
+#             return self.nombre
+#
+# class Usuario(models.Model):
+#     nombre = models.CharField(blank=False, max_length=15)
+#     apellidos = models.CharField(blank=False, max_length=30)
+#     phone_regex = RegexValidator(regex=r'^\d{9,15}$', message="Telefono debe ser formato: 99911122.")
+#     telefono = models.CharField(validators=[phone_regex], blank=True,max_length=15) # validators should be a list
+#     direccion = models.CharField(blank=True,max_length=100)
+#
+#     def __unicode__(self):
+#             return self.nombre+" "+self.apellidos
+#
+# class Recurso(models.Model):
+#     titulo = models.CharField(blank=False, max_length=100)
+#     autor = models.CharField(blank=True, max_length=15)
+#     descripcion = models.CharField(blank=True, max_length=100)
+#     orden = models.CharField(blank=True, max_length=10)
+#     genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+#     edicion = models.IntegerField(blank=True, null=True)
+#     dvd = models.BooleanField(default=False)
+#     def __unicode__(self):
+#             return self.titulo
+#
+# class Prestamo(models.Model):
+#
+#     fechaInicio = models.DateField(default=datetime.datetime.today)
+#     fechaFin = models.DateField(default=datetime.datetime.today()+datetime.timedelta(days=7)) #por defecto 7 dias despues de la creacion
+#     Recurso = models.OneToOneField(Recurso,unique=True)
+#     usuarioPrestamo = models.OneToOneField(Usuario,unique=True)
+#
+#     def __unicode__(self):
+#         return self.fechaFin.strftime("%Y-%m-%d")+" "+self.Recurso.__unicode__()+" "+self.usuarioPrestamo.__unicode__()
+# """
