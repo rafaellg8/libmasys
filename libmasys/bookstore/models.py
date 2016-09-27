0133e57bf7da13e482cd23c90f2a04421cf5e6ea
rafaellg8 <rafaellg8@correo.ugr.es>
2016-09-27 18:30:21 +0200

    heroku despliegue remoto

--- libmasys/bookstore/models.py
+++ libmasys/bookstore/models.py
@@ -30,7 +30,7 @@ class Recurso(models.Model):
     titulo = models.CharField(blank=False, max_length=200)
     autor = models.CharField(blank=False, max_length=300)
     descripcion = models.CharField(blank=True, max_length=250)
-    codigo = models.CharField(blank=True, max_length=10,unique=True)
+    codigo = models.CharField(blank=True, max_length=15,unique=True)
     estanteria = models.CharField(blank=True, max_length=100)
     genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
     anio = models.IntegerField(blank=True, null=True)
