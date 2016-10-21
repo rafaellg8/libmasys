22db9943192874c9b6aaa84f9a66d5048862cd28
Rafael Lachica Garrido <rafaellg8@users.noreply.github.com>
2016-10-21 09:41:35 +0200

    Quito contraseñas seguridad

--- libmasys/libmasys/settings.py
+++ libmasys/libmasys/settings.py
@@ -88,15 +88,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'bookstore.sqlite3'),
-        # 'USER': 'dsxvqbmrennrrl',
-        # 'PORT': '5432',
-        # 'PASSWORD': 'RLPEFYRwDTix_NNv0XBf5FqiOH',
-        # 'HOST': 'ec2-184-73-196-82.compute-1.amazonaws.com',
+        # 'USER': 'pepito',
+        # 'PORT': '1234',
+        # 'PASSWORD': 'password',
+        # 'HOST': 'host url',
     }
 }
 
 db_from_env = dj_database_url.config()
-DATABASES['default'] = dj_database_url.config(default='postgres://sxnryzoiyxbqqw:vxX9wG1Sw4ji1HqreAYU5_BQyu@ec2-54-163-239-218.compute-1.amazonaws.com:5432/d79971dk2mu5ba')
+DATABASES['default'] = dj_database_url.config(default='postgres://')
 
 
 
